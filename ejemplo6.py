afb731134e9c6bf23d3dd0869acad785d90713ef
ZAIR JEAN PIERE PAREDES TAMAYO <60424355@certus.edu.pe>
2026-09-03 21:49:54 -0500

    Guardando ejemplo6

---
+++ ejemplo6.py
@@ -0,0 +1,18 @@
+import keyboard
+import time
+
+porcentaje = 0
+
+print("¡Robot encendido! Presiona la tecla ESC para detenerlo.\n")
+
+while True:
+    porcentaje += 25
+    print(f"CARGANDO... {porcentaje}%")
+    time.sleep(5)
+
+    # Si el robot siente que presionaste 'esc', se rompe el bucle
+    if keyboard.is_pressed('esc'):
+        print("\n¡Tocaste la tecla ESC! El robot se detuvo.")
+        break
+
+print("Fin del juego.")
