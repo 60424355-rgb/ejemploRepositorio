f43be11ca655c2e964f7fb479eb023992280764a
ZAIR JEAN PIERE PAREDES TAMAYO <60424355@certus.edu.pe>
2026-09-10 20:21:15 -0500

    ejercicio con funciones

---
+++ fun3.py
@@ -0,0 +1,16 @@
+#Reescribiendo el ejemplo1 con funciones DEF
+#FUNCION PROCEDINTEL: NO DEVUELVE RESULTADOS
+#Solo ejecuta la taera
+def detectarTemperatura(lectura,consigna):
+    if lectura == consigna:
+        return true 
+    else:
+        #Acciones si es falso.
+        return false 
+detectarTemperatura(10,18)
+print(detectarTemperatura(25,30))
+if detectarTemperatura(30,50):
+    print("el resultado es verdadero")
+else:
+    print("el resultado es falso")
+#DETECTAR TEMPERATURA(22,-4)
